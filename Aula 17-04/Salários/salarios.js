877317f03f0b696b4dc691373b7f64ce5bb5b692
zaffarani.kaique@gmail.com <zaffarani.kaique@gmail.com>
2024-04-18 00:40:48 -0300

    erros corrigidos

--- Aula 17-04/Salários/salarios.js
+++ Aula 17-04/Salários/salarios.js
@@ -1,30 +1,32 @@
 function calcular() {
 var input = document.getElementById("input").value;
 var button = document.getElementById("botao");
-}
+
+const salario = "dev jr";
 
 switch(input) {
-    case "Dev Jr":
+    case "dev jr":
         console.log("Você receberá R$3.000,00")
         break;
 
-    case "Dev Senior":
+    case "dev senior":
         console.log("Você receberá R$6.000,00")
         break;
 
-    case "Pleno":
+    case "pleno":
         console.log("Você receberá R$12.000,00")
         break;
 
-    case "Tech lead":
+    case "tech lead":
         console.log("Você receberá R$24.000,00")
         break;
 
-    case "Diretor":
+    case "diretor":
         console.log("Você receberá R$48.000,00")
         break;
 
     default:
         console.log('Item inválido.')
         break;
+}
 }
