f7f192eab121cbb23c535c41891b34472860005f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-08 21:10:06 -0300

    aula 6

---
+++ Aula 6/array.js
@@ -0,0 +1,6 @@
+
+
+const pessoas = ["Caio", "Fulano", "Ciclano", 15, true,]
+console.log(pessoas[2])
+
+
---
+++ Aula 6/jokeipo.js
@@ -0,0 +1,34 @@
+
+function escolhaComp(){
+    // Função que tem 3 valores
+    const jokeipo = ["Pedra", "Papel", "Tesoura"]
+    // Variável que arrendonda para baixo o valor gerado aleatoriamente de acordo com o tamanho da array jokeipo[]
+    let numeroAleatorio = Math.floor(Math.random() * 3)
+    // Retorna a posição de acordo com o número aleatório
+    return jokeipo[numeroAleatorio]
+}
+
+function jogar(escolhaDoUsuario){
+
+    let escolhaPc = escolhaComp()
+    let resultado
+
+    if (escolhaPc === escolhaDoUsuario){
+        resultado = "Empatou!";
+        document.getElementById("resultado").style.color = "black";
+    }
+
+    else if(
+        (escolhaDoUsuario === "Pedra" && escolhaPc === "Tesoura") ||
+        (escolhaDoUsuario === "Papel" && escolhaPc === "Pedra") ||
+        (escolhaDoUsuario === "Tesoura" && escolhaPc === "Papel")
+    ){
+        resultado = "Você ganhou!";
+        document.getElementById("resultado").style.color = "green";
+    } else {
+        resultado = "Computador ganhou!";
+        document.getElementById("resultado").style.color = "red";
+    }
+
+    document.getElementById("resultado").innerText = resultado
+}
