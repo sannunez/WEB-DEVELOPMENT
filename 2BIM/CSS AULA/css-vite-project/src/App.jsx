ac99fd10715c6e9a9ca266d224c5d66730eba57a
LuizHNR <luizhneri12@gmail.com>
2024-09-04 21:12:27 -0300

    aula13

---
+++ 2BIM/CSS AULA/css-vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1 className="text-primaryFiap text-5xl ml-10 font-bold mb-2 pl-3 flex justify-center md:text-green-400"> Titulo </h1> 
+      
+    </>
+  )
+}
+
+export default App
+  
